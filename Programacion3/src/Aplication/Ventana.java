9f440040c8e7ce022d07734720d714184ab34501
yonatanaguirre16 <yaguirre_16@alu.uabcs.mx>
2025-02-25 00:14:26 -0700

    Unidad 2 - Ejercicio 19

--- Programacion3/src/Aplication/Ventana.java
+++ Programacion3/src/Aplication/Ventana.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -13,8 +14,11 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
-
+import javax.swing.UIManager;
 import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 
@@ -24,6 +28,10 @@ public class Ventana extends JFrame {
 	// definir fuente para utilizar solo la palabra etiqueta al usar esta fuente
 	Font fuenteGrande = new Font("Times New Roman", Font.BOLD, 35);
 	Font fuenteEtiquetas = new Font("Times New Roman", Font.BOLD, 20);
+	Font fuenteLoginTitle = new Font("Dejavu Sans Mono", Font.ITALIC, 30);
+	Font fuenteLoginSub = new Font("Dejavu Sans Mono", Font.CENTER_BASELINE, 20);
+	Font fuenteLoginSmaller = new Font("Dejavu Sans Mono", Font.CENTER_BASELINE, 10);
+
 
 	public Ventana(String title) {
 		
@@ -32,16 +40,18 @@ public class Ventana extends JFrame {
 		this.setVisible(true);
 		this.setLayout(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //termina el programa al cerrar
-		this.setSize(300, 420); // Tamaño del frame
+		this.setSize(1000, 500); // Tamaño del frame
 		this.setResizable(false); // decide si cambia de tamaño la ventana o no
 		this.setLocationRelativeTo(null); // centra la ventana 
 		
 		this.setMinimumSize(new Dimension(400, 400));
 		this.setMaximumSize(new Dimension(800, 1200));
 		
-		//this.add(this.login());
+		this.add(this.login());
+		
 		//this.add(this.registro());
-		this.add(this.calculadora());
+		//this.add(this.calculadora());
+		//this.add(this.tablas());
 		this.repaint();
 		
 		
@@ -50,77 +60,117 @@ public class Ventana extends JFrame {
 	
 	public JPanel login() {
 		
+		
+		JLabel labelImagen = new JLabel(new ImageIcon("login.png"));
+		labelImagen.setSize(500, 500);
+		labelImagen.setLocation(510, 20);
+		
 		JPanel login = new JPanel();
 		login.setLocation(0, 0);
-		login.setSize(500, 500); 
+		login.setSize(1000, 500); 
 		login.setOpaque(true);
-		login.setBackground(Color.gray);
+		login.setBackground(Color.BLACK);
 		login.setVisible(true);
 		login.setLayout(null);
 		
+		login.add(labelImagen);
+		
+
+		
 		JLabel etiqueta1 = new JLabel("Bienvenido");
-		etiqueta1.setSize(220, 65);
+		etiqueta1.setSize(220, 50);
 		etiqueta1.setLocation(150, 10);
-		etiqueta1.setBackground(Color.red);
 		etiqueta1.setForeground(Color.WHITE);
-		etiqueta1.setOpaque(true); // para backgrounds
-		etiqueta1.setFont(fuenteGrande);
-		etiqueta1.setHorizontalAlignment(JLabel.CENTER); // alinea las cosas al centro
+		etiqueta1.setFont(fuenteLoginTitle);
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER); 
 		
-		login.add(etiqueta1); // agrega la etiqueta1 al panel login
+		login.add(etiqueta1); 
 		
 		
-		JLabel etiqueta2 = new JLabel("Correo: ");
-		etiqueta2.setBounds(65, 90, 80, 30); // junta size y location en uno solo
-		etiqueta2.setBackground(Color.WHITE);
-		etiqueta2.setOpaque(true);
-		etiqueta2.setFont(fuenteEtiquetas);
+		JLabel etiqueta2 = new JLabel("Correo ");
+		etiqueta2.setBounds(140, 90, 80, 30);
+		etiqueta2.setForeground(Color.WHITE);
+		etiqueta2.setFont(fuenteLoginSub);
 		
 		login.add(etiqueta2);
 		
 		JTextField email = new JTextField();
-		email.setBounds(65, 125, 250, 30); // junta size y location en uno solo
+		email.setBounds(140, 125, 250, 30); 
 		// x ,y width height
 		email.setBackground(Color.WHITE);
 		email.setOpaque(true);
 		email.setFont(fuenteEtiquetas);
 		login.add(email);
 		
-		JLabel etiqueta3 = new JLabel("Password: ");
-		etiqueta3.setBounds(65, 180, 120, 30); // junta size y location en uno solo
-		etiqueta3.setBackground(Color.WHITE);
-		etiqueta3.setOpaque(true);
-		etiqueta3.setFont(fuenteEtiquetas);
-		
+		JLabel etiqueta3 = new JLabel("Contraseña");
+		etiqueta3.setBounds(140, 180, 120, 30); 
+		etiqueta3.setForeground(Color.WHITE);
+		etiqueta3.setFont(fuenteLoginSub);
 		login.add(etiqueta3);
 		
 		JPasswordField pass = new JPasswordField();
-		pass.setBounds(65, 215, 250, 30); // junta size y location en uno solo
-		// x ,y width height
+		pass.setBounds(140, 215, 250, 30); 
 		pass.setBackground(Color.WHITE);
 		pass.setOpaque(true);
 		pass.setFont(fuenteEtiquetas);
 		login.add(pass);
 		
 		JCheckBox recuerdame = new JCheckBox("Recuerdame");
-		recuerdame.setBounds(65, 265, 250, 30); // junta size y location en uno solo
+		recuerdame.setBounds(140, 2140, 250, 30); 
 		recuerdame.setOpaque(false);
 		recuerdame.setFont(new Font("Times New Roman" , Font.BOLD, 15));
 		login.add(recuerdame);
 		
-		JButton recordarContra = new JButton("¿olvidaste tu contraseña?");
-		recordarContra.setBounds(200, 265, 240, 30); // junta size y location en uno solo
+		/*JButton recordarContra = new JButton("¿olvidaste tu contraseña?");
+		recordarContra.setBounds(200, 2140, 240, 30); // junta size y location en uno solo
 	    recordarContra.setOpaque(false);
 		recordarContra.setBorderPainted(false);
 		recordarContra.setFont(new Font("Times New Roman" , Font.BOLD, 15));
-		login.add(recordarContra);
+		login.add(recordarContra);*/
 		
 		JButton access = new JButton("Acceder");
-		access.setBounds(180, 320, 120, 30); 
+		access.setBounds(210, 280, 120, 30); 
 		access.setBackground(Color.WHITE);
 		access.setOpaque(true);
 		access.setFont(fuenteEtiquetas);
-		etiqueta1.setHorizontalAlignment(JButton.CENTER); // alinea las cosas al centro
+		access.setHorizontalAlignment(JButton.CENTER); // alinea las cosas al centro
+		
+		access.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		    	access.setBackground(Color.BLUE);
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		    	access.setBackground(UIManager.getColor("control"));
+		    }
+		});
+		
+		JLabel registerText = new JLabel("¿No tienes una cuenta? Registrate");
+		registerText.setSize(370, 50);
+		registerText.setLocation(80, 340);
+		registerText.setForeground(Color.WHITE);
+		registerText.setFont(fuenteLoginSmaller);
+		registerText.setHorizontalAlignment(JLabel.CENTER); 
+		login.add(registerText);
+		
+		JButton register = new JButton("Registrate");
+		register.setBounds(200, 380, 140, 30); 
+		register.setBackground(Color.WHITE);
+		register.setOpaque(true);
+		register.setFont(fuenteLoginSub);
+		register.setHorizontalAlignment(JButton.CENTER); 
+		login.add(register);
+		
+		register.addMouseListener(new java.awt.event.MouseAdapter() {
+		    public void mouseEntered(java.awt.event.MouseEvent evt) {
+		        register.setBackground(Color.BLUE);
+		    }
+
+		    public void mouseExited(java.awt.event.MouseEvent evt) {
+		        register.setBackground(UIManager.getColor("control"));
+		    }
+		});
+
 
 		login.add(access);
 		
@@ -142,7 +192,7 @@ public class Ventana extends JFrame {
 		registro.setLayout(null);
 		
 		JLabel etiqueta1 = new JLabel("Registro");
-		etiqueta1.setSize(220, 65);
+		etiqueta1.setSize(220, 140);
 		etiqueta1.setLocation(150, 10);
 		etiqueta1.setBackground(Color.black);
 		etiqueta1.setForeground(Color.WHITE);
@@ -172,7 +222,7 @@ public class Ventana extends JFrame {
 		registro.add(email);
 		
 		JLabel etiqueta3 = new JLabel("BIO ");
-		etiqueta3.setBounds(190, 165, 120, 30); // junta size y location en uno solo
+		etiqueta3.setBounds(190, 1140, 120, 30); // junta size y location en uno solo
 		//etiqueta3.setBackground(Color.WHITE);
 		etiqueta3.setOpaque(false);
 		etiqueta3.setFont(fuenteEtiquetas);
@@ -200,7 +250,7 @@ public class Ventana extends JFrame {
 		registro.add(preferencias);
 		
 		JCheckBox pref1 = new JCheckBox("Dulce");
-		pref1.setBounds(65, 320, 80, 30); // junta size y location en uno solo
+		pref1.setBounds(140, 320, 80, 30); // junta size y location en uno solo
 		pref1.setOpaque(false);
 		pref1.setFont(new Font("Times New Roman" , Font.BOLD, 15));
 		registro.add(pref1);
@@ -220,7 +270,7 @@ public class Ventana extends JFrame {
 		pref3.setBorder(border);
 		
 		JLabel term = new JLabel("TÉRMINOS ");
-		term.setBounds(130, 365, 250, 30); // junta size y location en uno solo
+		term.setBounds(130, 3140, 250, 30); // junta size y location en uno solo
 		// x y, w h
 		term.setBackground(Color.yellow);
 		term.setOpaque(true);
@@ -271,13 +321,13 @@ public class Ventana extends JFrame {
 		
 		/*
 		JCheckBox recuerdame = new JCheckBox("Recuerdame");
-		recuerdame.setBounds(65, 265, 250, 30); // junta size y location en uno solo
+		recuerdame.setBounds(140, 2140, 250, 30); // junta size y location en uno solo
 		recuerdame.setOpaque(false);
 		recuerdame.setFont(new Font("Times New Roman" , Font.BOLD, 15));
 		login.add(recuerdame);
 		
 		JButton recordarContra = new JButton("¿olvidaste tu contraseña?");
-		recordarContra.setBounds(200, 265, 240, 30); // junta size y location en uno solo
+		recordarContra.setBounds(200, 2140, 240, 30); // junta size y location en uno solo
 	    recordarContra.setOpaque(false);
 		recordarContra.setBorderPainted(false);
 		recordarContra.setFont(new Font("Times New Roman" , Font.BOLD, 15));
@@ -325,140 +375,140 @@ public class Ventana extends JFrame {
 		calculadora.add(espacioNumeros);
 		
 		JButton borrarTodo = new JButton("CE");
-		borrarTodo.setBounds(42, 65, 65, 50);
+		borrarTodo.setBounds(42, 140, 140, 50);
 		borrarTodo.setBackground(Color.ORANGE);
 		borrarTodo.setOpaque(true);
 		borrarTodo.setFont(fuenteEtiquetas);
 		calculadora.add(borrarTodo);
 		
 		JButton botonVacio1 = new JButton();
-		botonVacio1.setBounds(120, 65, 65, 50);
+		botonVacio1.setBounds(120, 140, 140, 50);
 		botonVacio1.setBackground(Color.GRAY);
 		botonVacio1.setOpaque(true);
 		botonVacio1.setFont(fuenteEtiquetas);
 		calculadora.add(botonVacio1);
 		
 		JButton botonVacio2 = new JButton();
-		botonVacio2.setBounds(200, 65, 65, 50);
+		botonVacio2.setBounds(200, 140, 140, 50);
 		botonVacio2.setBackground(Color.GRAY);
 		botonVacio2.setOpaque(true);
 		botonVacio2.setFont(fuenteEtiquetas);
 		calculadora.add(botonVacio2);
 		
 		JButton botonVacio3 = new JButton();
-		botonVacio3.setBounds(278, 65, 65, 50);
+		botonVacio3.setBounds(278, 140, 140, 50);
 		botonVacio3.setBackground(Color.GRAY);
 		botonVacio3.setOpaque(true);
 		botonVacio3.setFont(fuenteEtiquetas);
 		calculadora.add(botonVacio3);
 		
 		JButton boton7 = new JButton("7"); // 7
-		boton7.setBounds(42, 127, 65, 50);
+		boton7.setBounds(42, 127, 140, 50);
 		boton7.setBackground(Color.GRAY);
 		boton7.setOpaque(true);
 		boton7.setFont(fuenteEtiquetas);
 		calculadora.add(boton7);
 		
 		JButton boton8 = new JButton("8"); // 8
-		boton8.setBounds(120, 127, 65, 50);
+		boton8.setBounds(120, 127, 140, 50);
 		boton8.setBackground(Color.GRAY);
 		boton8.setOpaque(true);
 		boton8.setFont(fuenteEtiquetas);
 		calculadora.add(boton8);
 		
 		JButton boton9 = new JButton("9"); // 9
-		boton9.setBounds(200, 127, 65, 50);
+		boton9.setBounds(200, 127, 140, 50);
 		boton9.setBackground(Color.GRAY);
 		boton9.setOpaque(true);
 		boton9.setFont(fuenteEtiquetas);
 		calculadora.add(boton9);
 		
 		JButton botonSlash = new JButton("/"); // /
-		botonSlash.setBounds(278, 127, 65, 50);
+		botonSlash.setBounds(278, 127, 140, 50);
 		botonSlash.setBackground(Color.ORANGE);
 		botonSlash.setOpaque(true);
 		botonSlash.setFont(fuenteEtiquetas);
 		calculadora.add(botonSlash);
 		
 		JButton boton4 = new JButton("4"); // 4
-		boton4.setBounds(42, 192, 65, 50);
+		boton4.setBounds(42, 192, 140, 50);
 		boton4.setBackground(Color.GRAY);
 		boton4.setOpaque(true);
 		boton4.setFont(fuenteEtiquetas);
 		calculadora.add(boton4);
 		
 		JButton boton5 = new JButton("5"); // 5
-		boton5.setBounds(120, 192, 65, 50);
+		boton5.setBounds(120, 192, 140, 50);
 		boton5.setBackground(Color.GRAY);
 		boton5.setOpaque(true);
 		boton5.setFont(fuenteEtiquetas);
 		calculadora.add(boton5);
 		
 		JButton boton6 = new JButton("6"); // 6
-		boton6.setBounds(200, 192, 65, 50);
+		boton6.setBounds(200, 192, 140, 50);
 		boton6.setBackground(Color.GRAY);
 		boton6.setOpaque(true);
 		boton6.setFont(fuenteEtiquetas);
 		calculadora.add(boton6);
 		
 		JButton botonMult = new JButton("*"); // *
-		botonMult.setBounds(278, 192, 65, 50);
+		botonMult.setBounds(278, 192, 140, 50);
 		botonMult.setBackground(Color.ORANGE);
 		botonMult.setOpaque(true);
 		botonMult.setFont(fuenteEtiquetas);
 		calculadora.add(botonMult);
 		
 		JButton boton1 = new JButton("1"); // 1
-		boton1.setBounds(42, 256, 65, 50);
+		boton1.setBounds(42, 256, 140, 50);
 		boton1.setBackground(Color.GRAY);
 		boton1.setOpaque(true);
 		boton1.setFont(fuenteEtiquetas);
 		calculadora.add(boton1);
 		
 		JButton boton2 = new JButton("2"); // 2
-		boton2.setBounds(120, 256, 65, 50);
+		boton2.setBounds(120, 256, 140, 50);
 		boton2.setBackground(Color.GRAY);
 		boton2.setOpaque(true);
 		boton2.setFont(fuenteEtiquetas);
 		calculadora.add(boton2);
 		
 		JButton boton3 = new JButton("3"); // 3
-		boton3.setBounds(200, 256, 65, 50);
+		boton3.setBounds(200, 256, 140, 50);
 		boton3.setBackground(Color.GRAY);
 		boton3.setOpaque(true);
 		boton3.setFont(fuenteEtiquetas);
 		calculadora.add(boton3);
 		
 		JButton botonMenos = new JButton("-"); // -
-		botonMenos.setBounds(278, 256, 65, 50);
+		botonMenos.setBounds(278, 256, 140, 50);
 		botonMenos.setBackground(Color.ORANGE);
 		botonMenos.setOpaque(true);
 		botonMenos.setFont(fuenteEtiquetas);
 		calculadora.add(botonMenos);
 		
 		JButton boton0 = new JButton("0"); // 0
-		boton0.setBounds(42, 320, 65, 50);
+		boton0.setBounds(42, 320, 140, 50);
 		boton0.setBackground(Color.GRAY);
 		boton0.setOpaque(true);
 		boton0.setFont(fuenteEtiquetas);
 		calculadora.add(boton0);
 		
 		JButton botonDot = new JButton("."); // .
-		botonDot.setBounds(120, 320, 65, 50);
+		botonDot.setBounds(120, 320, 140, 50);
 		botonDot.setBackground(Color.GRAY);
 		botonDot.setOpaque(true);
 		botonDot.setFont(fuenteEtiquetas);
 		calculadora.add(botonDot);
 		
 		JButton botonIgual = new JButton("="); // =
-		botonIgual.setBounds(200, 320, 65, 50);
+		botonIgual.setBounds(200, 320, 140, 50);
 		botonIgual.setBackground(Color.ORANGE);
 		botonIgual.setOpaque(true);
 		botonIgual.setFont(fuenteEtiquetas);
 		calculadora.add(botonIgual);
 		
 		JButton botonMas = new JButton("+"); // +
-		botonMas.setBounds(278, 320, 65, 50);
+		botonMas.setBounds(278, 320, 140, 50);
 		botonMas.setBackground(Color.ORANGE);
 		botonMas.setOpaque(true);
 		botonMas.setFont(fuenteEtiquetas);
@@ -473,5 +523,106 @@ public class Ventana extends JFrame {
 		return calculadora;
 	}
 	
-	
+	public JPanel tablas() {
+		JPanel tablas = new JPanel();
+		
+		tablas.setLocation(0, 0);
+		tablas.setSize(900, 750); 
+		tablas.setOpaque(true);
+		tablas.setBackground(Color.gray);
+		tablas.setVisible(true);
+		tablas.setLayout(null);
+		
+		JLabel etiqueta1 = new JLabel("USUARIOS");
+		etiqueta1.setSize(220, 60);
+		etiqueta1.setLocation(340, 10);
+		etiqueta1.setBackground(Color.BLACK);
+		etiqueta1.setForeground(Color.WHITE);
+		etiqueta1.setOpaque(true);
+		etiqueta1.setFont(fuenteGrande);
+		etiqueta1.setHorizontalAlignment(JLabel.CENTER); // alinea las cosas al centro
+		tablas.add(etiqueta1);
+		
+		JTextArea usuarios = new JTextArea("     \n         Total de Usuarios: \n                    100");
+		usuarios.setBounds(100, 100, 250, 80);
+		usuarios.setBackground(Color.BLACK);
+		usuarios.setForeground(Color.WHITE);
+		usuarios.setOpaque(true);
+		usuarios.setFont(fuenteEtiquetas);
+		//usuarios.setHorizontalAlignment(JTextArea.CENTER_ALIGNMENT);
+		tablas.add(usuarios);
+
+		JButton exportar = new JButton("Exportar");
+		exportar.setBounds(600, 160, 100, 30); 
+		exportar.setOpaque(false);
+		exportar.setBorderPainted(false);
+		exportar.setFont(new Font("Times New Roman" , Font.BOLD, 15));
+		tablas.add(exportar);
+		
+		JButton añadir = new JButton("Añadir");
+		añadir.setBounds(730, 160, 100, 30); 
+		añadir.setOpaque(false);
+		añadir.setBorderPainted(false);
+		añadir.setFont(new Font("Times New Roman" , Font.BOLD, 15));
+		tablas.add(añadir);
+		
+        
+        Object data[][]  = { {"1", "Yonatan", "yona@alasldasld", 12, "La Paz"}, 
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"},
+        		{"2", "Yonatan", "yona@alasldasld", 12, "La Paz"}
+        	
+        		
+        };
+		
+        String columnNames[] = {"ID",
+            	"Nombre",
+            	"Correo Electronico",
+            	"Edad",
+            	"Estado"
+            };
+        
+        JTable newTable = new JTable(data, columnNames);
+        JScrollPane sp = new JScrollPane(newTable);
+        
+        sp.setBounds(100, 250, 700, 400);
+
+        tablas.add(sp);
+
+      
+		
+		return tablas;
+		
+		
+		
+	}
 }
