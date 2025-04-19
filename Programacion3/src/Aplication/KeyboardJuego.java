93199a4aebce7f1d5accd62c0bddfe561492ab88
yonatan.deleon <yonatan.deleon@LVMX-7m4iUZlvGI.ari.network>
2025-04-19 13:36:34 -0700

    Unidad 3 - Ejercicio 31: Juego de laberinto terminado

--- Programacion3/src/Aplication/KeyboardJuego.java
+++ Programacion3/src/Aplication/KeyboardJuego.java
@@ -5,11 +5,19 @@ import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -19,9 +27,11 @@ import javax.swing.Timer;
 
 
 public class KeyboardJuego implements KeyListener {
+	
+    private final int TILE_SIZE = 28; 
+    private Clip clip;
 
     private JFrame frame;
-    public int x = 200, y = 200;
     private PaintPanel panel_2;
     Graphics2D g2;
     public Player sombra;
@@ -29,8 +39,57 @@ public class KeyboardJuego implements KeyListener {
     private int segundos = 0;
     private int lastPressed = 0;
     private int minutos = 0;
-    public int anchoFigura = 50, altoFigura = 50;
+    public int anchoFigura = 49, altoFigura = 49;
     Timer cronometro, cronometro2;
+    private int nivel;
+
+    // 1 = pared, 2 = meta, 3 = inicio, 4 = victoria
+    int[][] laberinto = {
+    	    {1,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,1,1,0,0,1},
+    	    {1,0,1,0,1,1,0,1,1,1,1,1,0,1,0,1,0,0,0,0,0,0,1},
+    	    {1,0,1,0,0,1,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,0,1},
+    	    {1,0,1,1,0,1,1,1,0,1,1,1,1,1,1,1,0,0,0,0,1,0,1},
+    	    {1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,0,1,0,1},
+    	    {1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,0,0,1,0,0,1,0,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,1,0,1,0,0,1,0,1,1,0,1},
+    	    {1,1,1,0,1,1,0,1,0,1,1,0,1,0,1,1,0,1,0,1,0,0,1},
+    	    {1,0,0,0,0,1,0,1,0,0,1,0,1,0,0,1,0,1,0,1,1,0,1},
+    	    {1,0,1,1,0,1,0,1,1,0,1,1,1,0,1,0,0,1,0,1,0,0,1},
+    	    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,1,1,1,0,0,1},
+    	    {1,0,1,1,1,0,1,0,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1},
+    	    {1,0,1,0,1,0,1,0,1,0,0,0,1,0,1,1,0,1,1,0,1,0,1},
+    	    {1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1,1,1},
+    	    {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1},
+    	    {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,1,0,1},
+    	    {1,0,1,0,1,0,1,1,1,0,1,0,1,0,1,1,1,1,0,1,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,4,1},
+    	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1}
+    	};
+    
+                                                                       
+    int[][] laberinto2 = {
+    	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    	    {1,0,1,0,1,1,0,0,1,1,1,1,0,1,1,0,0,1,1,1,1,1,1},
+    	    {1,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,0,1},
+    	    {1,1,1,1,0,1,1,1,0,1,1,1,1,0,1,0,0,0,0,0,1,0,3},
+    	    {1,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1},
+    	    {1,0,1,1,1,1,0,1,1,1,1,1,1,0,0,0,1,0,1,0,0,0,1},
+    	    {1,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,1,0,1,1,1,1,1},
+    	    {1,1,1,0,1,1,0,1,0,1,1,0,1,0,0,0,1,0,1,0,0,0,1},
+    	    {1,0,0,0,0,1,0,1,0,0,1,0,1,0,1,1,0,0,1,0,0,0,1},
+    	    {1,0,1,1,0,1,0,1,1,0,1,1,1,0,1,0,0,0,1,1,1,0,1},
+    	    {1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1},
+    	    {1,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,1,1,0,0,1,0,1},
+    	    {1,0,1,1,1,0,1,0,1,0,1,0,1,0,1,0,0,1,1,1,1,0,1},
+    	    {1,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,1},
+    	    {2,0,0,1,0,0,1,0,1,0,1,0,1,0,1,0,1,1,1,1,1,0,1},
+    	    {1,4,1,1,1,0,1,0,1,0,1,0,1,0,1,0,0,0,0,1,0,0,1},
+    	    {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,1,1,0,1,1,0,0,1},
+    	    {1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    	    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+    	};
 
     private ArrayList<Player> obstaculos = new ArrayList<>();
 	private JLabel timerLbl;
@@ -57,6 +116,7 @@ public class KeyboardJuego implements KeyListener {
      */
     public KeyboardJuego() {
         initialize();
+        reproducirMusica(); 
     }
 
     /**
@@ -64,16 +124,12 @@ public class KeyboardJuego implements KeyListener {
      */
     private void initialize() {
         frame = new JFrame();
-        frame.setBounds(100, 100, 675, 675);
+        frame.setBounds(100, 100, 659, 674);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Keyboard Game");
+        frame.setTitle("Laberinto");
         frame.setIconImage(Toolkit.getDefaultToolkit().getImage(Paint.class.getResource("/Aplication/sonicicon.png")));
         frame.setLocationRelativeTo(null);
 
-        player = new Player(200, 200, 50, 50, Color.green); 
-        sombra = new Player(player.x, player.y, player.alto, player.ancho, Color.green); 
-        obstaculos.add(new Player(150, 350, 100, 60, Color.orange)); 
-        obstaculos.add(new Player(300, 100, 80, 80, Color.red)); 
 
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel, BorderLayout.NORTH);
@@ -89,16 +145,49 @@ public class KeyboardJuego implements KeyListener {
         btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 19));
         btnNewButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                player.x = 200;
-                player.y = 200;
-                sombra.x = player.x;
-                sombra.y = player.y;
-                lastPressed = 0;
-                panel_2.repaint();
-                resetTimer();
-                frame.requestFocusInWindow(); // re-gana el focus al frame para volver a mover desde el teclado
+            	if(nivel==1) { // reinicia la posicion del jugador si es el tablero 1 o 2
+                    player.x = 34;
+                    player.y = 35;
+                    sombra.x = player.x;
+                    sombra.y = player.y;
+                    lastPressed = 0;
+                    panel_2.repaint();
+                    resetTimer();
+                    frame.requestFocusInWindow(); 
+            	}else if(nivel ==2) {
+                    player.x = 593;
+                    player.y = 122;
+                    sombra.x = player.x;
+                    sombra.y = player.y;
+                    lastPressed = 0;
+                    panel_2.repaint();
+                    resetTimer();
+                    frame.requestFocusInWindow(); // re-gana el focus al frame para volver a mover desde el teclado
+            	}
+
             }
         });
+        
+        JButton btnNewButton_2 = new JButton("Nivel 1");
+        btnNewButton_2.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		nivel=1;
+        		cargarNivel(nivel);
+
+        	}
+        });
+        btnNewButton_2.setFont(new Font("Tahoma", Font.PLAIN, 19));
+        panel_1.add(btnNewButton_2);
+        
+        JButton btnNewButton_1 = new JButton("Nivel 2");
+        btnNewButton_1.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		nivel=2;
+        		cargarNivel(nivel);
+        	}
+        });
+        btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 19));
+        panel_1.add(btnNewButton_1);
         panel_1.add(btnNewButton);
 
         panel_2 = new PaintPanel();
@@ -107,7 +196,21 @@ public class KeyboardJuego implements KeyListener {
         frame.setFocusable(true);
         frame.addKeyListener(this);
         
-	    cronometro = new Timer(1000, new ActionListener() {
+        
+        Object[] opciones = {"Nivel 1", "Nivel 2"};
+
+        int seleccion = JOptionPane.showOptionDialog(frame,"Elige el nivel:", "Maze Puzzle", 
+        		JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, opciones, opciones[0] 
+        );
+        if (seleccion == 0) {
+            nivel = 1;
+            cargarNivel(nivel);
+        } else if (seleccion == 1) {
+            nivel = 2;
+            cargarNivel(nivel);
+        }
+        
+	cronometro = new Timer(1000, new ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
 	            segundos++;
 	            if (segundos == 60) {
@@ -118,15 +221,46 @@ public class KeyboardJuego implements KeyListener {
 	        }
 	    });
 	    
-	    cronometro2 = new Timer(8, new ActionListener() {
+	cronometro2 = new Timer(20, new ActionListener() {
 	        public void actionPerformed(ActionEvent e) {
 	        	//System.out.println("movimiento");
-	        	update();
+	        	update(); 
 	        }
 	    });
 }
-    
 
+    private void cargarNivel(int nivel) {
+        obstaculos.clear();
+        
+        int[][] lab = (nivel == 1) ? laberinto : laberinto2;
+        
+        if(nivel==1) {
+        	player = new Player(1 * TILE_SIZE + 5, 1 * TILE_SIZE , TILE_SIZE - 10, TILE_SIZE - 10, Color.green, "pacifico.png");
+        	sombra = new Player(player.x, player.y, player.ancho, player.alto, Color.green, null); 
+        }else if(nivel==2){
+        	player = new Player(1 * TILE_SIZE + 565, 1 * TILE_SIZE+85 , TILE_SIZE - 10, TILE_SIZE - 10, Color.green, "pacifico.png");
+        	sombra = new Player(player.x, player.y, player.ancho, player.alto, Color.green, null); 
+        }
+        
+        for (int fila = 0; fila < lab.length; fila++) {
+            for (int col = 0; col < lab[0].length; col++) {
+                if (lab[fila][col] == 1) { 
+                    obstaculos.add(new Player(col * TILE_SIZE, fila * TILE_SIZE, TILE_SIZE, TILE_SIZE, null, "tape.png"));
+                }
+                if (lab[fila][col] == 3) { 
+                    obstaculos.add(new Player(col * TILE_SIZE, fila * TILE_SIZE, TILE_SIZE, TILE_SIZE, null, "modelo.jpg"));
+                }
+                if (lab[fila][col] == 2) { 
+                    obstaculos.add(new Player(col * TILE_SIZE - 1, fila * TILE_SIZE - 1, TILE_SIZE, TILE_SIZE, null, "home.png"));
+                }
+            }
+        }
+
+        panel_2.repaint();
+        resetTimer();
+        frame.requestFocusInWindow();
+    }
+ 
 	private void resetTimer() {
 	    minutos = 0;
 	    segundos = 0;
@@ -139,8 +273,7 @@ public class KeyboardJuego implements KeyListener {
 	}
 
     @Override
-    public void keyTyped(KeyEvent e) {
-    }
+    public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {
@@ -148,7 +281,6 @@ public class KeyboardJuego implements KeyListener {
     	lastPressed = e.getKeyCode();
 	    cronometro.start();
 	    cronometro2.start();
-	    
 	    panel_2.repaint();
 	    update();
 
@@ -188,6 +320,7 @@ public class KeyboardJuego implements KeyListener {
                  movimiento = false;
                  break;
              }
+             
          }
 
          if (movimiento) {
@@ -197,24 +330,76 @@ public class KeyboardJuego implements KeyListener {
              player.x = sombra.x;
              player.y = sombra.y;
          }
-
+         
+         verificarVictoria();
          panel_2.repaint();
     	
     }
 
+    private void verificarVictoria() {
+
+        int[][] lab = (nivel == 1) ? laberinto : laberinto2;
+
+        int xInicio = player.x / TILE_SIZE;
+        int yInicio = player.y / TILE_SIZE;
+        int xFin = (player.x + player.ancho - 1) / TILE_SIZE; 
+        int yFin = (player.y + player.alto - 1) / TILE_SIZE;
+
+        for (int y = yInicio; y <= yFin; y++) {
+            for (int x = xInicio; x <= xFin; x++) {
+                if (y >= 0 && y < lab.length && x >= 0 && x < lab[0].length) {
+                    if (lab[y][x] == 4) { // gana si toca 4 en la matriz 
+                        cronometro.stop();
+                        cronometro2.stop();
+                        JOptionPane.showMessageDialog(frame, "¡A beber! Te tardaste: " + timerLbl.getText());
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+    
+    private void reproducirMusica() {
+        try {
+            File audioFile = new File(getClass().getResource("flor.wav").toURI());
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+                        clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+            
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    
     public void keyReleased(KeyEvent e) {
     }
 
     class Player {
+    	BufferedImage imagen = null;
         int x = 0, y = 0, alto = 0, ancho = 0;
         Color c;
+        
 
-        public Player(int x, int y, int alto, int ancho, Color c) {
+        public Player(int x, int y, int ancho, int alto, Color c, String rutaImagen){
             this.x = x;
             this.y = y;
             this.ancho = ancho;
             this.alto = alto;
             this.c = c;
+            
+            if (rutaImagen != null) {
+                try {
+                    imagen = ImageIO.read(getClass().getResource(rutaImagen));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        public Player(int x, int y, int ancho, int alto, Color c) {
+            this(x, y, ancho, alto, c, null); 
         }
 
         public Boolean colision(Player target) {
@@ -237,13 +422,22 @@ public class KeyboardJuego implements KeyListener {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
             g2 = (Graphics2D) g; 
-
+            
             g2.setColor(player.c);
-            g2.fillRect(player.x, player.y, player.ancho, player.alto); // jugador
+            if (player.imagen != null) {
+                g2.drawImage(player.imagen, player.x, player.y, player.ancho, player.alto, null);
+            } else {
+                g2.setColor(player.c);
+                g2.fillRect(player.x, player.y, player.ancho, player.alto);
+            }
 
-            for (Player pared : obstaculos) { // obstaculos actualmente pinta 2 porque son los que hay en el arraylist
-                g2.setColor(pared.c);
-                g2.fillRect(pared.x, pared.y, pared.ancho, pared.alto);
+            for (Player pared : obstaculos) {
+                if (pared.imagen != null) {
+                    g2.drawImage(pared.imagen, pared.x, pared.y, pared.ancho, pared.alto, null);
+                } else {
+                    g2.setColor(pared.c);
+                    g2.fillRect(pared.x, pared.y, pared.ancho, pared.alto);
+                }
             }
         }
     }
